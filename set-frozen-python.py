f801ea2c0455083b1bacf34642825a9979f81402
ilhanuhuy <104136717+ilhanuhuy@users.noreply.github.com>
2022-04-21 16:57:09 +0700

    Create set-frozen-python.py

---
+++ set-frozen-python.py
@@ -0,0 +1,6 @@
+conlist=['Dewi', 'Budi', 'Cici', 'Linda', 'Cici']
+print(conlist)
+conset={'Dewi', 'Budi', 'Cici', 'Linda', 'Cici'}
+print(conset)
+confrozenset=({'Dewi', 'Budi', 'Cici', 'Linda', 'Cici'})
+print(confrozenset)
